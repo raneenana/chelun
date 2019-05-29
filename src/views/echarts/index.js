77d692eb64a6ab5fd0c1417241e0076a9e7fa760
raneenana <17773439@qq.com>
2019-05-29 11:39:16 +0800

    “change Echarts

--- src/views/echarts/index.js
+++ src/views/echarts/index.js
@@ -5,46 +5,54 @@ export default class Index extends React.Component {
   componentDidMount() {
     const el = document.querySelector(".echarts")
     var myChart = echarts.init(el);
-    var data = []
-    for (var i = 0; i <= 360; i++) {
-      var t = i / 180 * Math.PI
-      var r = Math.sin(2 * t) * Math.cos(2 * t)
-      data.push([r, i])
-    }
+    // var data = []
+    // for (var i = 0; i <= 360; i++) {
+    //   var t = i / 180 * Math.PI
+    //   var r = Math.sin(2 * t) * Math.cos(2 * t)
+    //   data.push([r, i])
+    // }
     var option = {
-      title: {
-        text: '极坐标双数值轴'
-      },
-      // legend: {
-      //   data: ['line']
-      // },
-      polar: {
-        center: ['50%', '35%']
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross'
-        }
-      },
       angleAxis: {
-        type: 'value',
-        startAngle: 0
+        type: 'category',
+        data: ['其他资产', '房产', '交通运输工具', '土地使用权', '设备'],
+        z: 10
       },
       radiusAxis: {
-        min: 0
       },
+      polar: {
+      },
+      color:['orange','green'],
       series: [{
+        type: 'bar',
+        data: [0, 0, 0, 3, 5],
         coordinateSystem: 'polar',
-        name: 'line',
-        type: 'line',
-        showSymbol: false,
-        data: data
-      }],
-      animationDuration: 2000
+        animation:false,
+        name: 'A',
+        stack: 'a'
+      },
+       {
+        type: 'bar',
+        data: [5, 6, 5, 0, 0],
+        coordinateSystem: 'polar',
+        name: 'B',
+        animation:false,
+        stack: 'a'
+      }, 
+      // {
+      //   type: 'bar',
+      //   data: [1.5, 2.5, 3.5, 4.5, 3.5],
+      //   coordinateSystem: 'polar',
+      //   name: 'C',
+      //   stack: 'a'
+      // }
+    ],
+      // legend: {
+      //   show: true,
+      //   data: ['A', 'B', 'C']
+      // }
     };
     myChart.setOption(option);
-    
+
   }
   render() {
     return <div className="echarts">
